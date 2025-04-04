6238f5f003cf9b342e6599ad2dcd9e3122c137dc
KeertiShidramshettar <keertishettar2011@gmail.com>
2025-04-04 10:59:22 +0530

    Save

---
+++ Inhe.java
@@ -0,0 +1,30 @@
+package CIE;
+
+class Grandfather {
+    void print() {
+        System.out.println("I'm the Grandfather");
+    }
+}
+
+class Father extends Grandfather {
+    void print1() {
+        System.out.println("I'm the Father");
+    }
+
+    class Son extends Father {
+        void print2() {
+            System.out.println("I'm the Son");
+        }
+    }
+}
+
+public class Inhe {
+    public static void main(String[] args) {
+        Father father = new Father();
+        Father.Son mySon = father.new Son();
+        
+        mySon.print();   
+        mySon.print1();  
+        mySon.print2(); 
+    }
+}
